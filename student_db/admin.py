a58fc3e6c3643f8ede6129f09e399be6c96bd40c
nadi666 <115098441+nadi666@users.noreply.github.com>
2023-08-09 01:03:55 +0600

    Update admin.py

--- student_db/admin.py
+++ student_db/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Student, Teacher, Subject, Speciality, Plan, SubjectPlan
-
+from .models import Student
 admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Subject)
-admin.site.register(Speciality)
-admin.site.register(Plan)
-admin.site.register(SubjectPlan)
