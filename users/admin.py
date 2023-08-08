7442ea36c5dcbdb816f9bb91206aa161ba628636
nadi666 <115098441+nadi666@users.noreply.github.com>
2023-08-09 01:06:26 +0600

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -3,10 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from users.forms import UserCreationForm
 
-User = get_user_model()
 
-
-@admin.register(User)
 class UserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
@@ -14,4 +11,4 @@ class UserAdmin(UserAdmin):
             'fields': ('username', 'email', 'password1', 'password2'),
         }),
     )
-    add_form = UserCreationForm
+    add_form = UserCreationForm()
